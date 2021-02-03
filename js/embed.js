10aa71fb837aecd18ff6892e162d607a326c4a2f
Johannes Håkonsen <jowhannez@hotmail.com>
2021-02-03 16:08:50 +0100

    Update embed.js

--- js/embed.js
+++ js/embed.js
@@ -21,3 +21,34 @@
         }
     }
 })();
+
+Module:
+
+export default {
+    _embed: null,
+    init() {
+        this._embed = document.querySelector('[data-embed]');
+        if (!this._embed) {
+            return;
+        }
+
+        const url       = this._embed.getAttribute('data-embed');
+        const parsedUrl = this._createEmbedLink(url);
+
+        if (parsedUrl) {
+            this._embed.innerHTML = '<iframe class="embed__iframe" src="' + parsedUrl + '" frameborder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+        }
+    },
+    _createEmbedLink(url) {
+        const youtubeRegex = /https?:\/\/(?:[0-9A-Z-]+\.)?(?:youtu\.be\/|youtube(?:-nocookie)?\.com\S*?[^\w\s-])([\w-]{11})(?=[^\w-]|$)(?![?=&+%\w.-]*(?:['"][^<>]*>|<\/a>))[?=&+%\w.-]*/ig;
+        const vimeoRegex   = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/
+    
+        if (url.match(youtubeRegex)) {
+            return url.replace(youtubeRegex, 'https://www.youtube.com/embed/$1');
+        } else if (url.match(vimeoRegex)) {
+            return url.replace(vimeoRegex, 'https://player.vimeo.com/video/$5?autoplay=1&loop=1&autopause=0');
+        }
+    
+        return false;
+    }
+}
